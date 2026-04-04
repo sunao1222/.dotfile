fb808606ebc4ed2abdfe39d6439eb8ef5d66779b
sunao1222 <g024c1054@g.neec.ac.jp>
2026-04-05 02:17:20 +0900

    fix:niri-switcher削除

--- home-manager/modules/wm/wm.nix
+++ home-manager/modules/wm/wm.nix
@@ -13,24 +13,6 @@ in
     # inputs.elephant.homeManagerModules.default
   ];
 
-  programs.niriswitcher = {
-    enable = true;
-    settings = {
-      keys = {
-        modifier = "Super";
-        switch = {
-          next = "Tab";
-          prev = "Shift+Tab";
-        };
-      };
-      center_on_focus = true;
-      appearance = {
-        system_theme = "dark";
-        icon_size = 64;
-      };
-    };
-  };
-
   programs.walker = {
     enable = true;
     runAsService = true;
