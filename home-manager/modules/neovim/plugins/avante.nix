cca1c65a89843f6b533480230f1d95588b9070fe
sunao1222 <g024c1054@g.neec.ac.jp>
2026-04-15 14:42:59 +0900

    fix:avante.nvimのproviderをさくらのAIに

--- home-manager/modules/neovim/plugins/avante.nix
+++ home-manager/modules/neovim/plugins/avante.nix
@@ -4,18 +4,25 @@
       avante = {
         enable = true;
         settings = {
-          provider = "llama-cpp";
+          input = {
+            provider = "snacks";
+            provider_opts = {
+              title = "Avante Input";
+              icon = " ";
+            };
+          };
+          provider = "sakura";
           providers = {
-            llama-cpp = {
+            sakura = {
               __inherited_from = "openai";
-              endpoint = "http://ume-chan:8080/v1";
-              model = "Qwen3-Coder-30B-A3B-Instruct-Q4_K_M.gguf";
-              disable_tools = true;
+              endpoint = "https://api.ai.sakura.ad.jp/v1/chat/completions";
+              auth_type = "api";
+              # model = "Qwen3-Coder-480B-A35B-Instruct-FP8";
+              # timeout = 30000;
               # extra_request_body = {
-              #   max_tokens = 4096;
-              #   temperature = 0;
+              #   max_tokens = 20480;
+              #   temperature = 0.75;
               # };
-              # model = "claude-3-5-sonnet-20240620";
             };
           };
         };
