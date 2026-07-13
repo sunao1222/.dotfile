7c1230e6e06a4d4bf36bcb244789f39285cc1d42
sunao1222 <g024c1054@g.neec.ac.jp>
2026-07-14 00:07:10 +0900

    feat: Enable custom Alpha dashboard layout with terminal and buttons

--- home-manager/modules/neovim/plugins/alpha-nvim.nix
+++ home-manager/modules/neovim/plugins/alpha-nvim.nix
@@ -1,76 +1,76 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
 
   programs.nixvim = {
     plugins = {
       alpha = {
         enable = true;
-        # layout = [
-        #   {
-        #     type = "padding";
-        #     val = 4;
-        #   }
-        #   {
-        #     opts = {
-        #       redraw = true;
-        #     };
-        #     type = "terminal";
-        #     command = "${config.xdg.dataHome}/nvim/scripts/startup.sh";
-        #     width = 54;
-        #     height = 11;
-        #   }
-        #   {
-        #     type = "padding";
-        #     val = 2;
-        #   }
-        #   {
-        #     opt = {
-        #       "inherit" = {
-        #         position = "center";
-        #       };
-        #     };
-        #     type = "group";
-        #     val = [
-        #       {
-        #         on_press = {
-        #           __raw = "function() vim.cmd[[ene]] end";
-        #         };
-        #         opts = {
-        #           shortcut = "n";
-        #           position = "center";
-        #         };
-        #         type = "button";
-        #         val = " New file";
-        #       }
-        #       {
-        #         on_press = {
-        #           __raw = "function() vim.cmd[[qa]] end";
-        #         };
-        #         opts = {
-        #           # cursor = 2;
-        #           shortcut = "q";
-        #           position = "center";
-        #         };
-        #         type = "button";
-        #         val = "󰈆  > Quit NVIM";
-        #       }
-        #     ];
-        #   }
-        #   {
-        #     type = "padding";
-        #     val = 2;
-        #   }
-        #   {
-        #     opts = {
-        #       hl = "Keyword";
-        #       position = "center";
-        #     };
-        #     type = "text";
-        #     val = "Inspiring quote here.";
-        #   }
-        # ];
+        layout = [
+          {
+            type = "padding";
+            val = 4;
+          }
+          {
+            opts = {
+              redraw = true;
+            };
+            type = "terminal";
+            command = "${config.xdg.dataHome}/nvim/scripts/startup.sh";
+            width = 54;
+            height = 11;
+          }
+          {
+            type = "padding";
+            val = 2;
+          }
+          {
+            opt = {
+              "inherit" = {
+                position = "center";
+              };
+            };
+            type = "group";
+            val = [
+              {
+                on_press = {
+                  __raw = "function() vim.cmd[[ene]] end";
+                };
+                opts = {
+                  shortcut = "n";
+                  position = "center";
+                };
+                type = "button";
+                val = " New file";
+              }
+              {
+                on_press = {
+                  __raw = "function() vim.cmd[[qa]] end";
+                };
+                opts = {
+                  # cursor = 2;
+                  shortcut = "q";
+                  position = "center";
+                };
+                type = "button";
+                val = "󰈆  > Quit NVIM";
+              }
+            ];
+          }
+          {
+            type = "padding";
+            val = 2;
+          }
+          {
+            opts = {
+              hl = "Keyword";
+              position = "center";
+            };
+            type = "text";
+            val = "Inspiring quote here.";
+          }
+        ];
 
-        theme = "theta";
+        # theme = "theta";
       };
     };
 
